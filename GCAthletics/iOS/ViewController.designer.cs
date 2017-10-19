d6c15bce9bcdc149bbaf6d3f487d59d427a24466
dbernstein15 <dbernstein15@gmail.com>
2017-10-18 21:03:32 -0400

    Initial login page

--- GCAthletics/iOS/ViewController.designer.cs
+++ GCAthletics/iOS/ViewController.designer.cs
@@ -1,24 +1,66 @@
-﻿//		
-// This file has been generated automatically by MonoDevelop to store outlets and		
-// actions made in the Xcode designer. If it is removed, they will be lost.		
-// Manual changes to this file may not be handled correctly.		
-//		
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
-namespace GCAthletics.iOS
+namespace gcAthletics.iOS
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
         [Outlet]
         UIKit.UIButton Button { get; set; }
 
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton AlertButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Name { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Position { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Team { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Welcome { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
-            if (Button != null)
-            {
-                Button.Dispose();
-                Button = null;
+            if (AlertButton != null) {
+                AlertButton.Dispose ();
+                AlertButton = null;
+            }
+
+            if (Name != null) {
+                Name.Dispose ();
+                Name = null;
+            }
+
+            if (Position != null) {
+                Position.Dispose ();
+                Position = null;
+            }
+
+            if (Team != null) {
+                Team.Dispose ();
+                Team = null;
+            }
+
+            if (Welcome != null) {
+                Welcome.Dispose ();
+                Welcome = null;
             }
         }
     }
