e43668c4add56ca04399b988ca56082ebf13cf51
PotatoFuhrer <john.spehr@bobcats.gcsu.edu>
2017-10-24 22:19:04 -0400

    Added Barrett's files to testing
    Added all of Barrett's files to testing in order to attempt to manually merge barrett with testing

---
+++ GCAthletics/GCAthletics/Models/PasswordModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace GCAthletics
+{
+    public class PasswordModel
+    {
+        public string PasswordHash { get; set; }
+        public int UserID { get; set; }
+        public int IsInitial { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("[Password: PasswordHash={0}, UserID={1}, IsInitial={2}]", PasswordHash, UserID, IsInitial);
+        }
+    }
+}
---
+++ GCAthletics/GCAthletics/Models/WorkoutModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace GCAthletics
+{
+    public class WorkoutModel
+    {
+        public int WorkoutID { get; set; }
+        public string Date { get; set; }
+        public int Completed { get; set; }
+        public int UserID { get; set; }
+        public int EventID { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("[Workout: WorkoutID={0}, Date={1}, Completed={2}, UserID={3}, EventID={4}]", WorkoutID, Date, Completed, UserID, EventID);
+        }
+    }
+}
