a13b1faf8e0092bcc3f584ab5a0d10a0607e5577
PotatoFuhrer <john.spehr@bobcats.gcsu.edu>
2017-10-24 22:04:32 -0400

    fixed muh branch again
    reverted once more

---
+++ GCAthletics/Droid/AlertsActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GCAthletics.Droid
+{
+    [Activity(Label = "Activity1", MainLauncher = false)]
+    public class AlertsActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.AlertScreen);
+
+            // Create your application here
+        }
+
+        //when back button is pressed, go to log in screen
+        public override void OnBackPressed()
+        {
+            var intent = new Intent(this, typeof(HomeActivity));
+            StartActivity(intent);
+        }
+    }
+}
