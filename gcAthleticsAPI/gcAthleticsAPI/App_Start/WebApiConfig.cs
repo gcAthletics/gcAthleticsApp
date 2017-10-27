8fd84f5e9cda01a0b88dc4415f0657b02f819fc3
Barrett <awetstone56@gmail.com>
2017-10-27 00:24:11 -0400

    Included project in GCAthletics Solution

--- gcAthleticsAPI/gcAthleticsAPI/App_Start/WebApiConfig.cs
+++ gcAthleticsAPI/gcAthleticsAPI/App_Start/WebApiConfig.cs
@@ -17,7 +17,7 @@ namespace gcAthleticsAPI
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
 
